60fc8f38f6942f4ac65760f84d68af8ae95400a4
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-10-02 02:13:52 +0300

    task 3 done

--- task_1.py
+++ task_1.py
@@ -1,7 +1,6 @@
 # Реалізуйте цикл, який перебиратиме всі значення ітерабельного об'єкту iterable
-my_list = ["one", "piece", "per", "time"]
+my_list = ["one", "piece", "per", "time", "hello", "world"]
 my_iter = iter(my_list)
-
 # for item in my_iter:
 #     print(item)
 print(my_list)
@@ -15,8 +14,20 @@ for item in my_list:
 # print(next(my_iter))
 # print(next(my_iter))
 print("")
-index = 0
-while index < len(my_list):
-    print(next(my_iter))
-    index += 1
+# index = 0
+# while index < len(my_list):
+#     print(next(my_iter))
+#     index += 1
+# for i in range(len(my_list)):
+#     print(my_iter.__next__())
+print(my_iter.__dir__())
+print(my_iter.__iter__())
+print(my_iter.__str__())
+print(my_iter.__class__)
+print("")
+while True:
+    try:
+        print("--", next(my_iter))
+    except StopIteration:
+        break
 
--- task_3.py
+++ task_3.py
@@ -1,1 +1,21 @@
 # Напишіть ітератор, який повертає елементи заданого списку у зворотному порядку (аналог reversed).
+class Iterator:
+    def __init__(self, my_list: list):
+        self.my_list = my_list    
+
+    def __iter__(self):
+        self.index = len(self.my_list) - 1      
+        return self
+    
+    def __next__(self):
+        if self.index < 0:
+            raise StopIteration
+        result = f"{self.index + 1} -  {self.my_list[self.index]}"
+        self.index -= 1
+        return result
+    
+my_list = Iterator(["one", "piece", "per", "time"])
+
+for item in my_list:
+    print(item)
+
