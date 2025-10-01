f8ce47ad27d24bf56ea7c2a294b5d42ed74506b8
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-10-01 20:36:05 +0300

    task 1 done

--- task_1.py
+++ task_1.py
@@ -1,1 +1,22 @@
 # Реалізуйте цикл, який перебиратиме всі значення ітерабельного об'єкту iterable
+my_list = ["one", "piece", "per", "time"]
+my_iter = iter(my_list)
+
+# for item in my_iter:
+#     print(item)
+print(my_list)
+print(my_iter)
+print('')
+for item in my_list:
+    print(item)
+# print("")
+# print(next(my_iter))
+# print(next(my_iter))
+# print(next(my_iter))
+# print(next(my_iter))
+print("")
+index = 0
+while index < len(my_list):
+    print(next(my_iter))
+    index += 1
+
